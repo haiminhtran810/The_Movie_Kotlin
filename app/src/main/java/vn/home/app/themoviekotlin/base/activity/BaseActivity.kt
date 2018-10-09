38251ae710c5da2d3bcad2e4b3b83f44a9c0ff59
haiminhtran810 <haiminhtran810@gmail.com>
2018-10-09 14:54:53 +0700

    Add Fabric - Crashlytics

--- app/src/main/java/vn/home/app/themoviekotlin/base/activity/BaseActivity.kt
+++ app/src/main/java/vn/home/app/themoviekotlin/base/activity/BaseActivity.kt
@@ -1,9 +1,16 @@
 package vn.home.app.themoviekotlin.base.activity
 
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.crashlytics.android.Crashlytics
+import io.fabric.sdk.android.Fabric
 
 /**
  * Created by HaiMinhTran on 9/10/2018.
  */
 abstract class BaseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Fabric.with(this, Crashlytics())
+    }
 }
