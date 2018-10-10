a9748f4eafa2e86335a0eb2f8a165a70b1f2a161
haiminhtran810 <haiminhtran810@gmail.com>
2018-10-10 09:13:33 +0700

    Add Menu Popup

--- app/src/main/java/vn/home/app/themoviekotlin/ui/topSelling/TopSellingAdapter.kt
+++ app/src/main/java/vn/home/app/themoviekotlin/ui/topSelling/TopSellingAdapter.kt
@@ -1,8 +1,12 @@
 package vn.home.app.themoviekotlin.ui.topSelling
 
+import android.content.Context
 import android.databinding.DataBindingUtil
 import android.support.v7.util.DiffUtil
+import android.support.v7.widget.PopupMenu
+import android.view.Gravity
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.ViewGroup
 import vn.home.app.themoviekotlin.BuildConfig
 import vn.home.app.themoviekotlin.R
@@ -10,7 +14,8 @@ import vn.home.app.themoviekotlin.base.adapter.BaseRecyclerAdapter
 import vn.home.app.themoviekotlin.data.model.Movie
 import vn.home.app.themoviekotlin.databinding.ItemMovieBinding
 
-class TopSellingAdapter : BaseRecyclerAdapter<Movie, ItemMovieBinding>(DIFF_CALLBACK) {
+class TopSellingAdapter(
+        val itemClickMore: ((MenuItem) -> Unit)? = null, val itemClick: ((Movie) -> Unit)?) : BaseRecyclerAdapter<Movie, ItemMovieBinding>(DIFF_CALLBACK) {
 
     private companion object {
         private val DIFF_CALLBACK = object : DiffUtil.ItemCallback<Movie>() {
@@ -26,7 +31,8 @@ class TopSellingAdapter : BaseRecyclerAdapter<Movie, ItemMovieBinding>(DIFF_CALL
     }
 
     override fun createBinding(parent: ViewGroup, viewType: Int): ItemMovieBinding {
-        val binding = DataBindingUtil.inflate<ItemMovieBinding>(LayoutInflater.from(parent.context), R.layout.item_movie, parent, false)
+        val binding = DataBindingUtil.inflate<ItemMovieBinding>(
+                LayoutInflater.from(parent.context), R.layout.item_movie, parent, false)
         return binding
     }
 
@@ -37,7 +43,29 @@ class TopSellingAdapter : BaseRecyclerAdapter<Movie, ItemMovieBinding>(DIFF_CALL
             txtMoney.text = itemMovie.budget.toString() + "$"
             txtStt.text = positionItem.toString()
             url = BuildConfig.SMALL_IMAGE_URL + itemMovie.poster_path
+            root.setOnClickListener {
+                itemMovie?.apply {
+                    itemClick?.invoke(this)
+                }
+            }
+
+            imgOption.setOnClickListener {
+                itemMovie?.apply {
+                    //itemClickMore?.invoke(this)
+                    val popup = PopupMenu(binding.root.context, binding.imgOption, Gravity.RIGHT)
+                    //inflating menu from xml resource
+                    popup.inflate(R.menu.menu_popup)
+                    //adding click listener
+                    popup.setOnMenuItemClickListener {
+                        itemClickMore?.invoke(it)
+                        false
+                    }
+                    popup.show()
+                }
+            }
         }
+
+
     }
 
 
--- app/src/main/java/vn/home/app/themoviekotlin/ui/topSelling/TopSellingFragment.kt
+++ app/src/main/java/vn/home/app/themoviekotlin/ui/topSelling/TopSellingFragment.kt
@@ -4,11 +4,17 @@ package vn.home.app.themoviekotlin.ui.topSelling
 import android.arch.lifecycle.Observer
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.PopupMenu
+import android.view.MenuItem
 import org.koin.android.viewmodel.ext.android.viewModel
 import vn.home.app.themoviekotlin.BR
 import vn.home.app.themoviekotlin.base.fragment.BaseLoadMoreRefreshFragment
 import vn.home.app.themoviekotlin.data.model.Movie
 import vn.home.app.themoviekotlin.databinding.FragmentLoadMoreRefreshBinding
+import vn.home.app.themoviekotlin.R.id.menu3
+import vn.home.app.themoviekotlin.R.id.menu2
+import vn.home.app.themoviekotlin.R.id.menu1
+
 
 class TopSellingFragment : BaseLoadMoreRefreshFragment<FragmentLoadMoreRefreshBinding, TopSellingViewModel, Movie>(), TopSellingNavigator {
     companion object {
@@ -35,10 +41,18 @@ class TopSellingFragment : BaseLoadMoreRefreshFragment<FragmentLoadMoreRefreshBi
     }
 
     private fun setAdapterTopSelling() {
-        val topSellingAdapter = TopSellingAdapter()
+        val topSellingAdapter = TopSellingAdapter({ menuItem -> onClickItemMore(menuItem) }, { movie -> onClickItem(movie) })
         viewBinding.recyclerView.apply {
             layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
             adapter = topSellingAdapter
         }
+    }
+
+    fun onClickItemMore(menuItem: MenuItem) {
+        val a = menuItem
+    }
+
+    fun onClickItem(movie: Movie) {
+        val a = movie
     }
 }
